544d9048c5482f4ea50845d5d6d450ce6b9afc61
AntonyOnScript <santhiagoafk@gmail.com>
2022-07-31 12:22:20 -0300

    feat: boards core

--- src/views/Boards/BoardsPage.vue
+++ src/views/Boards/BoardsPage.vue
@@ -8,9 +8,11 @@ const labelBoards: Record<string, unknown> = reactive({
   finished: []
 })
 
-const heldLabelTitle: Ref<HTMLElement | null> = ref(null)
-const heldLabelDescription: Ref<HTMLElement | null> = ref(null)
 const heldLabel: Ref<null | undefined | Record<string, any>> = ref({})
+const heldLabelDescription: Ref<HTMLElement | null> = ref(null)
+const toDoLabelRef: Ref<HTMLElement | null> = ref(null)
+const inProgressLabelRef: Ref<HTMLElement | null> = ref(null)
+const finishedLabelRef: Ref<HTMLElement | null> = ref(null)
 
 function addItem(item: string) {
   const innerItem: any = labelBoards[item]
@@ -60,6 +62,49 @@ function updateItem(index: number, label: string) {
   labelBoards[label][index]['isEditing'] = false
 }
 
+function moveLabel(e, currentIndex, currentLabel) {
+  const clickPath = e.path
+  let clickedItem = clickPath.filter(node => {
+    if (node.className) {
+      return node.className.indexOf("listed-label") !== -1 ? true : false
+    }
+    return false
+  })
+  clickedItem = clickedItem[0]
+  window.isMovingLabel = true
+  window.onmousemove = (event) => {
+    if (window.isMovingLabel) {
+      clickedItem.classList.add("label-backgrounded")
+      const { x, y } = event
+      const topValue = (y - (93 / 2))
+      const leftValue = (x - (402 / 2))
+      clickedItem.style.top = `${topValue}px`
+      clickedItem.style.left = `${leftValue}px`
+      clickedItem.style.position = "absolute"
+    } else {
+      window.onmousemove = {}
+      if (clickPath[0].tagName === "I" || clickPath[0].className.indexOf("q-btn") !== -1) return false
+      const labelBoardsRefs = [toDoLabelRef, inProgressLabelRef, finishedLabelRef]
+      const labelBoardsValues = labelBoardsRefs.map(label => label.value[0].getBoundingClientRect().x)
+      const clickedItemPositions = clickedItem.getBoundingClientRect()
+      const closestValue = labelBoardsRefs.reduce((prev, curr) => Math.abs(curr.value[0].getBoundingClientRect().x - clickedItemPositions.x) < Math.abs(prev.value[0].getBoundingClientRect().x - clickedItemPositions.x) ? curr : prev)
+      const droppedLabel = Object.keys(labelBoards)[labelBoardsValues.indexOf(closestValue.value[0].getBoundingClientRect().x)]
+      labelBoards[droppedLabel].push(labelBoards[currentLabel][currentIndex])
+      labelBoards[currentLabel].splice(currentIndex, 1)
+      clickedItem.classList.remove("label-backgrounded")
+      clickedItem.style.position = "relative"
+      clickedItem.style.top = ""
+      clickedItem.style.left = ""
+    }
+  }
+
+  window.onmouseup = () => {
+    if (window.isMovingLabel)
+      window.isMovingLabel = false
+  }
+}
+
+window.isMovingLabel = false
 </script>
 
 <template>
@@ -69,6 +114,7 @@ function updateItem(index: number, label: string) {
     <div
       v-for="(value, label, index) in labelBoards"
       :key="index"
+      :ref="`${label}LabelRef`"
       class="col"
     >
       <h6 class="my-4">
@@ -85,7 +131,6 @@ function updateItem(index: number, label: string) {
       >
         <q-card-section>
           <q-input
-            ref="heldLabelTitle"
             v-model="heldLabel.title"
             class="mb-2"
             placeholder="title"
@@ -105,11 +150,17 @@ function updateItem(index: number, label: string) {
         </q-card-section>
       </q-card>
       <q-card 
-        v-for="(item, index) in value.reverse()"
-        :key="index"
+        v-for="(item, index) in value"
+        style="position: unset"
+        :key="`${item['title']}-${index}`"
         class="mt-2"
       >
-        <q-card-section v-if="!item['isEditing']">
+        <q-card-section 
+          v-if="!item['isEditing']"
+          class="listed-label"
+          @mousedown="e => moveLabel(e, index, label)"
+        >
+          {{label}}
           <div
             class="row justify-between"
           >
@@ -126,9 +177,10 @@ function updateItem(index: number, label: string) {
             {{ item['description'] }}
           </p>
         </q-card-section>
-        <q-card-section v-else>
+        <q-card-section
+          v-else
+        >
           <q-input
-            ref="heldLabelTitle"
             v-model="item.title"
             class="mb-2"
             placeholder="title"
@@ -155,5 +207,16 @@ function updateItem(index: number, label: string) {
 <style scoped>
   .full-button {
     width: 100%;
+  }
+
+  .listed-label {
+    cursor: pointer;
+    width: 402px;
+  }
+
+  .label-backgrounded {
+    z-index: 2;
+    background: white;
+    box-shadow: 0 1px 5px rgb(0 0 0 / 20%), 0 2px 2px rgb(0 0 0 / 14%), 0 3px 1px -2px rgb(0 0 0 / 12%);  
   }
 </style>
