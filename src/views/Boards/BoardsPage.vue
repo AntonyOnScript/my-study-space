0193b68f964ac12713e549442f950ea129ede669
AntonyOnScript <santhiagoafk@gmail.com>
2022-08-02 09:48:33 -0300

    draft commit

--- src/views/Boards/BoardsPage.vue
+++ src/views/Boards/BoardsPage.vue
@@ -66,6 +66,10 @@ function updateItem(index: number, label: string) {
   labelBoards[label][index]['isEditing'] = false
 }
 
+function createBoardLabel() {
+  labelBoards['teste'] = []
+}
+
 function moveLabel(e, currentIndex, currentLabel) {
   const clickPath = e.path
   if (clickPath[0].tagName === "I" || clickPath[0].className.indexOf("q-btn") !== -1) return false
@@ -116,118 +120,131 @@ window.isMovingLabel = false
 </script>
 
 <template>
-  <div
-    class="row gap-10 px-10"
-  >
+  <div>
+    <div class="row justify-end px-10">
+      <div class="row column mt-4">
+        <span>Adicionar coluna</span>
+        <q-btn
+          padding="5px 30px"
+          icon="add"
+          size="xs"
+          @click="createBoardLabel"
+        />
+      </div>
+    </div>
     <div
-      v-for="(value, label, index) in labelBoards"
-      :key="index"
-      :ref="`${label}LabelRef`"
-      class="col"
+      class="row gap-10 px-10 mt-6"
     >
-      <h6 class="my-4">
-        {{ label }}
-      </h6>
-      <q-btn
-        padding="5px 30px"
-        icon="add"
-        class="full-button"
-        @click="createHeldLabel(label)"
-      />
-      <q-card 
-        v-if="heldLabel?.label === label"
+      <div
+        v-for="(value, label, index) in labelBoards"
+        :key="index"
+        :ref="`${label}LabelRef`"
+        class="col"
       >
-        <q-card-section>
-          <q-input
-            v-model="heldLabel.title"
-            class="mb-2"
-            placeholder="title"
-            autofocus
-            outlined
-            @blur="e => focusDescription(e)"
-            @keydown="e => focusDescription(e)"
-          />
-          <q-input
-            ref="heldLabelDescription"
-            v-model="heldLabel.description"
-            placeholder="description"
-            outlined
-            @blur="createItem(label)"
-            @keydown="e => e.key === 'Enter' ? createItem(label) : ''"
-          />
-        </q-card-section>
-      </q-card>
-      <q-card 
-        v-for="(item, index) in value"
-        style="position: unset"
-        :key="`${item['title']}-${index}`"
-        class="mt-2"
-      >
-        <q-card-section 
-          v-if="!item['isEditing']"
-          class="listed-label"
-          @mousedown="e => moveLabel(e, index, label)"
+        <h6 class="my-4">
+          {{ label }}
+        </h6>
+        <q-btn
+          padding="5px 30px"
+          icon="add"
+          class="full-button"
+          @click="createHeldLabel(label)"
+        />
+        <q-card 
+          v-if="heldLabel?.label === label"
         >
-          <div class="row justify-between">
-            {{label}} 
-            <div class="no-flex-height">
-              <q-btn
-                size="sm"
-                icon="create"
-                class="ml-2"
-                @click="editLabel(index, label)"
-                flat
-              />
-              <q-btn
-                size="sm"
-                icon="delete"
-                class="ml-2"
-                @click="removeLabel(index, label)"
-                flat
-              />
-            </div>
-          </div>
-          <div
-            class="row no-wrap mb-4"
+          <q-card-section>
+            <q-input
+              v-model="heldLabel.title"
+              class="mb-2"
+              placeholder="title"
+              autofocus
+              outlined
+              @blur="e => focusDescription(e)"
+              @keydown="e => focusDescription(e)"
+            />
+            <q-input
+              ref="heldLabelDescription"
+              v-model="heldLabel.description"
+              placeholder="description"
+              outlined
+              @blur="createItem(label)"
+              @keydown="e => e.key === 'Enter' ? createItem(label) : ''"
+            />
+          </q-card-section>
+        </q-card>
+        <q-card 
+          v-for="(item, index) in value"
+          style="position: unset"
+          :key="`${item['title']}-${index}`"
+          class="mt-2"
+        >
+          <q-card-section 
+            v-if="!item['isEditing']"
+            class="listed-label"
+            @mousedown="e => moveLabel(e, index, label)"
           >
-            <h6 
-              class="mb-0 mt-0 col-8" 
+            <div class="row justify-between">
+              {{label}} 
+              <div class="no-flex-height">
+                <q-btn
+                  size="sm"
+                  icon="create"
+                  class="ml-2"
+                  @click="editLabel(index, label)"
+                  flat
+                />
+                <q-btn
+                  size="sm"
+                  icon="delete"
+                  class="ml-2"
+                  @click="removeLabel(index, label)"
+                  flat
+                />
+              </div>
+            </div>
+            <div
+              class="row no-wrap mb-4"
+            >
+              <h6 
+                class="mb-0 mt-0 col-8" 
+                style="word-break: break-all"
+              >
+                {{ item['title'] }}
+              </h6>
+            </div>
+            <p 
+              class="mb-0" 
               style="word-break: break-all"
             >
-              {{ item['title'] }}
-            </h6>
-          </div>
-          <p 
-            class="mb-0" 
-            style="word-break: break-all"
+              {{ item['description'] }}
+            </p>
+          </q-card-section>
+          <q-card-section
+            v-else
           >
-            {{ item['description'] }}
-          </p>
-        </q-card-section>
-        <q-card-section
-          v-else
-        >
-          <q-input
-            v-model="item.title"
-            class="mb-2"
-            placeholder="title"
-            autofocus
-            outlined
-            @blur="e => focusDescription(e)"
-            @keydown="e => focusDescription(e)"
-          />
-          <q-input
-            ref="heldLabelDescription"
-            v-model="item.description"
-            placeholder="description"
-            outlined
-            @blur="updateItem(index, label)"
-            @keydown="e => e.key === 'Enter' ? updateItem(index, label) : ''"
-          />
-        </q-card-section>
-      </q-card>
+            <q-input
+              v-model="item.title"
+              class="mb-2"
+              placeholder="title"
+              autofocus
+              outlined
+              @blur="e => focusDescription(e)"
+              @keydown="e => focusDescription(e)"
+            />
+            <q-input
+              ref="heldLabelDescription"
+              v-model="item.description"
+              placeholder="description"
+              outlined
+              @blur="updateItem(index, label)"
+              @keydown="e => e.key === 'Enter' ? updateItem(index, label) : ''"
+            />
+          </q-card-section>
+        </q-card>
+      </div>
+      <code style="position: absolute; bottom: 0;">{{ heldLabel }}</code>
     </div>
-    <code style="position: absolute; bottom: 0;">{{ heldLabel }}</code>
   </div>
 </template>
 
